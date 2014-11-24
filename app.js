fa93c1697ffea59bc56c723bcdc89ea28fba07d2
stefan judis <stefanjudis@gmail.com>
2014-11-24 23:40:54 +0100

    improved env keys + added data for newly merged resources

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ var config      = {
     list  : './templates/list.tpl'
   },
   twitter : {
-    consumer_key        : process.env.CONSUMER_KEY,
-    consumer_secret     : process.env.CONSUMER_SECRET,
-    access_token        : process.env.ACCESS_TOKEN,
-    access_token_secret : process.env.ACCESS_TOKEN_SECRET
+    consumer_key        : process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret     : process.env.TWITTER_CONSUMER_SECRET,
+    access_token        : process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret : process.env.TWITTER_ACCESS_TOKEN_SECRET
   },
   vimeo   : {
     clientId     : process.env.VIMEO_CLIENT_ID,
